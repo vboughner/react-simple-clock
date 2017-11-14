018727c18dce50809afeeebd066477ad57dfeb15
Van Boughner <van.boughner@ntti3.com>
2017-11-14 15:22:35 -0800

    Using a javascript style for a button

--- src/ButtonLand.js
+++ src/ButtonLand.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class ButtonLand extends Component {
+
+    static BUTTON_STYLES =  {
+        border: '2px solid red',
+    };
+
     render() {
         return (
             <div className="button-land">
-                <button onClick={this.props.onClick}>Update Props {this.props.counter}</button>
+                <button style={ButtonLand.BUTTON_STYLES} onClick={this.props.onClick}>Update Props {this.props.counter}</button>
                 {
                     (this.props.hasClock) ?
                         <button onClick={this.props.onRemoveClock}>Remove Clock</button> :
